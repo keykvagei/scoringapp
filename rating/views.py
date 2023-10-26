ebfd72b9a64795e4664523c5e49489f8a6670da6
unknown <mohammad.erfan.gooneh@gmail.com>
2023-10-26 13:41:42 +0330

    end of second day

--- rating/views.py
+++ rating/views.py
@@ -7,34 +7,40 @@ from profiles.models import Profile
 from posts.models import Post
 from rating.models import Rate
 
+
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def rating_view(request):
-    if request.method == "POST":
-        target_id = request.POST.get("target")
-        post_id = request.POST.get("post")
-        rate = Decimal(request.POST.get("rate"))
+    if request.method != "POST":
+        return HttpResponse("Invalid request method")
+    
+    print("XXXXXXXXX: ", request.POST)
+    target_id = request.POST.get("target")
+    post_id = request.POST.get("post")
+    rate = Decimal(request.POST.get("rate"))
 
 
-        target = get_object_or_404(Profile, unique_id=target_id)
-        sender = request.user
-        if post_id:
-            post = get_object_or_404(Post, pk=post_id)
+    target = get_object_or_404(Profile, unique_id=target_id)
+    sender = request.user
+    if post_id:
+        post = get_object_or_404(Post, pk=post_id)
 
-        if rate >= 0 and rate <= 5:
-            Profile.objects.filter(unique_id=target_id).update(
+    if rate >= 0 and rate <= 5:
+        Profile.objects.filter(unique_id=target_id).update(
+            total_rating_value=F('total_rating_value') + rate,
+            total_ratings=F('total_ratings') + 1,
+            rate = (F('rate') * F('total_rating_value') + rate * sender.rate)/(F('total_rating_value') + sender.rate)
+        )
+        if post:
+            post.update(
                 total_rating_value=F('total_rating_value') + rate,
                 total_ratings=F('total_ratings') + 1,
-                rate = (F('rate') * F('total_rating_value') + rate * sender.rate)/(F('total_rating_value') + sender.rate)
+                post_rate = (F('post_rate') * F('total_rating_value') + rate * sender.rate)/(F('total_rating_value') + sender.rate)
             )
-            if post:
-                post.update(
-                    total_rating_value=F('total_rating_value') + rate,
-                    total_ratings=F('total_ratings') + 1,
-                    post_rate = (F('post_rate') * F('total_rating_value') + rate * sender.rate)/(F('total_rating_value') + sender.rate)
-                )
 
-            Rate.objects.create(sender=sender, target=target, amount=rate)
+        Rate.objects.create(sender=sender, target=target, amount=rate)
 
-            return HttpResponse("Rating updated successfully")
+        return HttpResponse("Rating updated successfully")
 
-        return HttpResponse("Invalid rating value")
-    return HttpResponse("Invalid request method")
+    return HttpResponse("Invalid rating value")
